b3015709af7c817f62914d5ac0b5b06c6ddb15d0
Upeksha1214 <upekshasachintha@gmail.com>
2022-09-29 03:49:35 -0700

    updated

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Register from './screen/Register'
+import Login from './screen/Login'
+
+
+
+export default function App() {
+  return (
+    <Login/>
+  )
+}
---
+++ screen/Register.js
@@ -0,0 +1,34 @@
+import {StyleSheet} from 'react-native'
+import React from 'react'
+import { NativeBaseProvider, Box ,Stack ,Input, Button } from "native-base";
+
+export default function Register() {
+  return (
+    <NativeBaseProvider>
+        <Stack space={4} w="75%" maxW="300px" mx="auto" style={{marginTop:'10%'}}>
+        <Input style={{backgroundColor:'red', }} size="xl" placeholder="User Name" />
+        </Stack> 
+        <Stack space={4} w="75%" maxW="300px" mx="auto" style={{marginTop:'5%'}}>
+        <Input style={{backgroundColor:'red', }} size="xl" placeholder="User Name" />
+        </Stack>
+        <Stack space={4} w="75%" maxW="300px" mx="auto" style={{marginTop:'5%'}}>
+        <Input style={{backgroundColor:'red', }} size="xl" placeholder="Password" />
+        </Stack>
+        <Stack space={4} w="75%" maxW="300px" mx="auto" style={{marginTop:'5%'}}>
+        <Input style={{backgroundColor:'red', }} size="xl" placeholder="Confirm" />
+        </Stack>
+
+        <Stack space={4} w="75%" maxW="300px" mx="auto" style={{marginTop:'5%' ,}}>
+        <Button colorScheme="success">Register</Button>
+        </Stack>
+          
+    </NativeBaseProvider>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  input:{
+    backgroundColor:'red'
+  }
+})
